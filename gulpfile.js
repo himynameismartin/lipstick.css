2ec9013feef3a9d539198788cca55f5b65262a4f
himynameismartin <himynameismartin@gmail.com>
2016-12-18 14:41:53 +0100

    feat(lipstick): move .less to /src folder and output to /dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 
 gulp.task('less', function() {
-    return gulp.src('lipstick.less')
+    return gulp.src('src/lipstick.less')
         .pipe(less())
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('lipstick.css')
+    return gulp.src('dist/lipstick.css')
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['less', 'minify-css']);
