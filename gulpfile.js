15db5b5f7a9ff8e7b991021bb0ee858af9857e54
himynameismartin <himynameismartin@gmail.com>
2017-01-08 19:28:43 +0100

    feat(gulp): add cssbeautify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var cssbeautify = require('gulp-cssbeautify');
 var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 
@@ -9,11 +10,17 @@ gulp.task('less', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', ['less'], function() {
+gulp.task('beautify-css', ['less'], function() {
+    return gulp.src('dist/lipstick.css')
+        .pipe(cssbeautify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', ['less', 'beautify-css'], function() {
     return gulp.src('dist/lipstick.css')
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['less', 'minify-css']);
+gulp.task('default', ['less', 'beautify-css', 'minify-css']);
